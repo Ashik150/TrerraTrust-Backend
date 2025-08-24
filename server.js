5c8dac7cd62f53f317cbb456e597580d707859f6
Shat-El-Shahriar Khan <123257380+Ashik150@users.noreply.github.com>
2025-08-24 08:01:58 +0600

    Increase JSON and URL-encoded body size limit

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit:"5mb"}));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cors());
 app.use(helmet());
 connectDB();
